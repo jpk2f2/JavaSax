73d2bacfe2d8fd1688828e48b3d184877ec040da
Jason Kayser <jpk2f2@mail.missouri.edu>
2018-03-02 22:14:34 -0600

    Additional changes. Printing still not functional

--- JavaSax/src/javasax/FXMLDocumentController.java
+++ JavaSax/src/javasax/FXMLDocumentController.java
@@ -9,6 +9,9 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -40,8 +43,8 @@ public class FXMLDocumentController implements Initializable {
         if (file != null) {
             try {
                 System.out.println(file.getName());
-                SaxLoader.load(file);
-                
+                SaxNode root = SaxLoader.load(file);
+                //this.textArea.setText(null);
             } catch (Exception ex) {
                 displayExceptionAlert("Exception parsing XML file.", ex);
             }
@@ -53,11 +56,12 @@ public class FXMLDocumentController implements Initializable {
         displayAboutAlert();
     }
     
+    
     private void displayAboutAlert() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("About");
         alert.setHeaderText("XML DOM Parser Example");
-        alert.setContentText("This application was developed by Dale Musser for CS4330 at the University of Missouri.");
+        alert.setContentText("This application was developed by Jason Kayser for CS4330 at the University of Missouri based off in part work done by Dale Musser.");
         
         TextArea textArea = new TextArea("This example illustrates how to parse XML using the DOM.");
         textArea.setEditable(false);
